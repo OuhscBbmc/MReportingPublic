302d17c34a6bc195905caed69ad5860206e2890d
Will Beasley <wibeasley@hotmail.com>
2016-11-11 22:07:42 -0600

    Starting retention Shiny report

---
+++ Shiny/retention/server.R
@@ -0,0 +1,166 @@
+# ---- load-packages  -----------------------------------
+library(shiny)
+library(htmlwidgets)
+library(magrittr)
+library(scales)
+library(ggplot2)
+requireNamespace("grid")
+requireNamespace("DT")
+
+# ---- declare-globals  -----------------------------------
+# path_input_server_outside <- "//bbmc-shiny-public/Anonymous/MReportingPublic/UpcomingSchedule.csv"
+# path_input_server_inside  <- "/var/shinydata/MReportingPublic/UpcomingSchedule.csv"
+path_input_repo             <- "../.././DataPhiFreeCache/Raw/C1/eto-visit.rds"
+
+#Define some common cosmetics for the report.
+report_theme <- theme_light() +
+  theme(axis.text         = element_text(colour="gray40")) +
+  theme(axis.title        = element_text(colour="gray40")) +
+  theme(panel.border      = element_rect(colour="gray80")) +
+  theme(axis.ticks        = element_line(colour="gray80")) +
+  theme(axis.ticks.length = grid::unit(0, "cm"))
+
+# move_to_last <- function(data, move) { #http://stackoverflow.com/questions/18339370
+#   data[c(setdiff(names(data), move), move)]
+# }
+# 
+# status_levels <- c("0" = "Due Date", "1" = "Scheduled", "2" = "Confirmed", "3" = "Cancelled", "4" = "No Show")
+# icons_status <- c("Due Date"="bicycle", "Scheduled"="book", "Confirmed"="bug", "Cancelled"="bolt", "No Show"="ban")
+# order_status  <- as.integer(names(status_levels)); names(order_status) <- status_levels
+
+
+ActivityEachMonth <- function( dsPlot, responseVariable, 
+  monthVariable="ActivityMonth", groupVariable="RegionTag", colorVariable=groupVariable, 
+  highlightedRegions = character(0),
+  # highlightedRegions="ftj",
+  mainTitle=NULL, xTitle=NULL, yTitle=NULL, baseSize=8, palette=NULL ) {
+  # library(mgcv, quietly=TRUE) #For the Generalized Additive Model that smooths the longitudinal graphs.
+ 
+  g <- ggplot(dsPlot, aes_string(x=monthVariable, y=responseVariable, colour=colorVariable, group=groupVariable))
+  g <- g + aes(ymin=0)
+  g <- g + geom_boxplot(aes_string(group=monthVariable), color=NA, fill="gray50", outlier.colour=NA, na.rm=TRUE, width=3, alpha=.3)
+  g <- g + geom_line(stat="identity", alpha=.5)
+  g <- g + geom_point(stat="identity", shape=21, alpha=.5)
+  # g <- g + geom_hline(yintercept=median(dsPlot[, responseVariable]), color="gray50")
+  # g <- g + geom_smooth(aes(group=1), method="gam", formula=y ~ s(x, bs = "cs"), color="gray30", na.rm=TRUE)
+  g <- g + scale_x_date(breaks="1 month", labels=date_format("%Y\n%b"))
+
+  if( (highlightedRegions != "All") & (length(highlightedRegions)==1) ) {
+    dsHighlight <- dsPlot[dsPlot$RegionTag %in% highlightedRegions, ]
+    dsLabelLeft <- dsHighlight[dsHighlight[, monthVariable]==min(dsHighlight[, monthVariable], na.rm=T), ]
+    dsLabelRight <- dsHighlight[dsHighlight[, monthVariable]==max(dsHighlight[, monthVariable], na.rm=T), ]
+
+    g <- g + geom_text(mapping=aes_string(label=colorVariable), data=dsLabelLeft, size=5, hjust=1.4) #Left endpoint
+    g <- g + geom_text(mapping=aes_string(label=colorVariable), data=dsLabelRight, size=5, hjust=-.4) #Right endpoint   
+    g <- g + geom_line(data=dsHighlight, stat="identity", alpha=1, size=2, na.rm=TRUE)
+  }
+  
+#   if( !is.null(palette) ) 
+#     g <- g + scale_color_manual(values=palette)	
+  g <- g + guides(colour="none")
+  g <- g + labs(title=mainTitle, x=xTitle, y=yTitle)
+  g <- g + reportTheme
+  return( g )
+}
+# ActivityEachMonth(readRDS("./DataPhiFree/Derived/C1/C1CountyMonth.rds"), "VisitCount")
+
+
+# ---- load-data -----------------------------------
+# if( file.exists(pathUpcomingScheduleServerOutside) ) {
+#   pathUpcomingSchedule <- pathUpcomingScheduleServerOutside  
+# } else if( file.exists(pathUpcomingScheduleServerInside) ) {
+#   pathUpcomingSchedule <- pathUpcomingScheduleServerInside  
+# } else {
+pathC1RegionMonth <- pathC1RegionMonthRepo
+# }
+
+dsC1RegionMonth <- readRDS(pathC1RegionMonth) 
+
+# ---- tweak-data -----------------------------------
+
+# Define a server for the Shiny app  -----------------------------------
+function(input, output) {
+  
+  FilterRegionMonth <- function( start_date=as.Date("2000-01-01"), stop_date=as.Date("2100-12-12")) {# Filter schedule based on selections
+    d <- dsC1RegionMonth
+    
+    if( nrow(d)>0 & input$regionTag != "All" )
+      d <- d[d$RegionTag==input$regionTag, ]
+   
+    d <- d[(start_date<=d$ActivityMonth) & (d$ActivityMonth<=stop_date), ]
+    return( d )
+  }  
+  FilterMonth <- function( start_date=as.Date("2000-01-01"), stop_date=as.Date("2100-12-12")) {# Filter schedule based on selections
+    d <- dsC1RegionMonth
+    d <- d[(start_date<=d$ActivityMonth) & (d$ActivityMonth<=stop_date), ]
+    return( d )
+  }
+
+  output$ScheduleTablePast <- DT::renderDataTable({
+    d <- FilterRegionMonth(start_date=input$dateRange[1], stop_date=input$dateRange[2])
+    
+    d <- d %>%
+      dplyr::mutate(
+        ActivityMonth = strftime(ActivityMonth, "%Y-%m"),
+        VisitsPerInfantNeed = round(VisitsPerInfantNeed, 3)
+      ) %>%
+      dplyr::select(
+        -WicNeedPopInfant
+      ) %>%
+      dplyr::rename_(
+        "Region Tag" = "RegionTag",
+        "Month" = "ActivityMonth",
+        "Visit Count" = "VisitCount",
+        "Visits per Need" = "VisitsPerInfantNeed"
+      )
+    
+    DT::datatable(
+      d, 
+      rownames = FALSE,
+      style    = 'bootstrap', 
+      options  = list(
+        searching  = FALSE,
+        sort       = FALSE,
+        language   = list(emptyTable="--No data to populate this table.  Consider using less restrictive filters.--")#,
+        # rowCallback = JS( 
+        #   'function(row, data) {
+        #     if (data[3].indexOf("Interview") > -1 ) {
+        #       $("td", row).addClass("interviewRow"); 
+        #       $("td:eq(3)", row).addClass("interviewEvent"); 
+        #     } else if (data[3].indexOf("Reminder") > -1 ) {
+        #       $("td:eq(3)", row).addClass("reminderEvent"); 
+        #     }
+        #   }'
+        # )
+      ),
+      class        = 'table-striped table-condensed table-hover', #Applies Bootstrap styles, see http://getbootstrap.com/css/#tables
+      escape       = FALSE
+    ) #%>%
+    # formatStyle(
+    #   columns = 'Status', 
+    #   color = styleEqual(names(palette_status), palette_status)
+    # )
+  })  
+  output$GraphVisitCount <- renderPlot({
+    d <- FilterMonth(start_date=input$dateRange[1], stop_date=input$dateRange[2])
+    highlightedRegions <- input$regionTag
+    ActivityEachMonth(d, responseVariable="VisitCount", highlightedRegions=highlightedRegions, mainTitle="Visits Each Month (per region)") + 
+      scale_y_continuous(labels=comma_format())  
+  })
+  output$GraphVisitPerNeed <- renderPlot({
+    d <- FilterMonth(start_date=input$dateRange[1], stop_date=input$dateRange[2])
+    highlightedRegions <- input$regionTag
+    ActivityEachMonth(d, responseVariable="VisitsPerInfantNeed", highlightedRegions=highlightedRegions, mainTitle="Visits Each Month per WIC Need (per region)") + 
+      scale_y_continuous(labels=percent_format())  
+  })
+  output$table_file_info <- renderText({
+    return( paste0(
+      '<h3>Details</h3>',
+      "<table border='0' cellspacing='1' cellpadding='2' >",
+      "<tr><td>Data Path:<td/><td>&nbsp;", pathC1RegionMonth, "<td/><tr/>",
+      "<tr><td>Data Last Modified:<td/><td>&nbsp;", file.info(pathC1RegionMonth)$mtime, "<td/><tr/>",
+      "<tr><td>App Restart Time:<td/><td>&nbsp;", file.info("restart.txt")$mtime, "<td/><tr/>",
+      "<table/>"
+    ) )
+  })
+}
---
+++ Shiny/retention/ui.R
@@ -0,0 +1,103 @@
+# The ui.R file formats the screen seen by the user in the browser.
+
+# ---- load-packages  -----------------------------------
+library(shiny)
+library(shinydashboard)
+# library(DT)
+library(ggplot2)
+
+# ---- declare-globals  -----------------------------------
+#Header
+tags_style <- "
+  h3 {color:red; font-size:150%}
+  .panelHeader {color:#605CA8; font-size:200%}
+  .table .smallish {font-size:80%; padding:2px; }
+  .table .interviewEvent {color:#bb2288; background:#D8FFCC;}
+  .table .interviewRow {font-size:90%; font-weight:bold}
+"
+
+header <- dashboardHeader(
+  title = "C1 Activity",
+  dropdownMenuOutput("messageMenuPast"),
+  dropdownMenuOutput("messageMenuUpcoming")
+)
+
+dashboardPage(
+  skin   = "green",
+  header = header,
+  dashboardSidebar(
+    HTML('<i class="fa fa-filter panelHeader"> Filters</i>'),
+    selectInput(
+      inputId = "regionTag",
+      label   = "Region",
+      choices = list(
+        "All", 
+        "ao", "aq", "fm", "ii", "it", "jr", "kc", "pd", "qc", "qs", 
+        "rv", "sx", "tq", "ua", "uq", "wn", "wr", "yn", "zi", "zj"
+      ),
+      selected = "All"
+    ),    
+    dateRangeInput(
+      inputId    = 'dateRange', 
+      separator  = "-",
+      label      = 'Date Format: yyyy-mm-dd',
+      start      = as.Date("2015-01-01"), 
+      end        = lubridate::floor_date(Sys.Date(), "month")
+    ),
+    HTML('<i class="fa fa-camera panelHeader"> Views</i>'),
+    sidebarMenu(
+      menuItem("Table", tabName="table"),
+      menuItem("Referrals (coming)", tabName="referrals"),
+      menuItem("Enrollments (coming)", tabName="enrollments"),
+      menuItem("Visits", tabName="visits"),
+      menuItem("General Links", tabName="generalLinks")
+    )
+  ),  
+  dashboardBody(
+    shiny::tags$head(
+      includeCSS("./www/styles.css"), # Include our custom CSS
+      tags$style(HTML(tags_style))
+    ),#End tags$head 
+    h3("!!!Warning! This report is currently for demonstration purposes.  ETO is working on a ticket to fix some issues with the export of Visit records!!!", color="red"),
+    tabItems( #type = "tabs",
+      tabItem(
+        tabName = "table",
+        DT::dataTableOutput(outputId = "ScheduleTablePast"),
+        h3("!!!Warning! This report is currently for demonstration purposes.  ETO is working on a ticket to fix some issues with the export of Visit records!!!", color="red"),
+        HTML("<br/><font color='#605CA8'>This table (will) include prototypical indicators of all four best practices outcomes.",
+             "The current columns are: ",
+             "<table>",
+             "  <tr><td><code>Region Tag</code></td><td> a randomly-generated two-letter code for each region.</td></tr>", # <td> is standard cells that contain the data
+             "  <tr><td><code>Month</code></td><td> Month for the specific record (YYYY-MM).</td></tr>",
+             "  <tr><td><code>Referral Count</code></td><td> total number of monthly referrals. (<em>Coming soon</em>)</td></tr>",
+             "  <tr><td><code>Referrals per Need</code></td><td> rate of monthly referrals per 'infant in need'. (<em>Coming soon</em>)</td></tr>",
+             "  <tr><td><code>Enrollment Count</code></td><td> total number of monthly enrollments. (<em>Coming soon</em>)</td></tr>",
+             "  <tr><td><code>Enrollments per Referral</code></td><td> rate of monthly enrollments per referral. (<em>Coming soon</em>)</td></tr>",
+             "  <tr><td><code>Visit Count</code></td><td> total number of visits for the month.</td></tr>",
+             "  <tr><td><code>Visits per Enrollment</code></td><td> rate of monthly visits per enrollment. (<em>Coming soon</em>)</td></tr>",
+             "  <tr><td><code>Visits per Need</code></td><td> rate of monthly visits per 'infant in need'.</td></tr>",
+             "</table>",
+             "</font>")
+      ), #End the (first) tab with the 'table' table
+      tabItem(
+        tabName = "referrals",
+        HTML("<font color='#605CA8'>Referral information is coming soon.</font><br/>")
+      ), #End the (second) tab with the graph
+      tabItem(
+        tabName = "enrollments",
+        HTML("<font color='#605CA8'>Enrollment information is coming soon.</font><br/>")
+      ), #End the (third) tab with the graph
+      tabItem(
+        tabName = "visits", 
+        shiny::plotOutput(outputId = "GraphVisitCount", width='95%', height='400px'),
+        HTML("<br/>"),
+        shiny::plotOutput(outputId = "GraphVisitPerNeed", width='95%', height='400px')
+      ), #End the (fourth) tab with the graph
+      tabItem(
+        tabName = "generalLinks", 
+        HTML("<font color='green'>{<em>What explanatory text would be helpful here?</em>}</font><br/>"),
+        htmlOutput(outputId='table_file_info')
+      )#, #End the (six) tab with the links & details
+    ) #End the tabsetPanel
+  ) #End the dashboardBody
+) #End the dashboardPage
