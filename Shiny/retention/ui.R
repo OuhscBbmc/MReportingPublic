e41138dfa6697f9af94fb76da3d507577bcf2c3d
Will Beasley <wibeasley@hotmail.com>
2016-11-13 14:46:35 -0600

    Legend for column names

--- Shiny/retention/ui.R
+++ Shiny/retention/ui.R
@@ -60,20 +60,38 @@ dashboardPage(
       tabItem(
         tabName = "table",
         DT::dataTableOutput(outputId = "visit_table"),
-        HTML("<br/><font color='#605CA8'>This table (will) include prototypical indicators of all four best practices outcomes.",
-             "The current columns are: ",
+        HTML("<br/><font color='#605CA8'>Each row in this table describe's a completed home visit.",
+             "The columns are: ",
              "<table>",
-             "  <tr><td><code>Region Tag</code></td><td> a randomly-generated two-letter code for each region.</td></tr>", # <td> is standard cells that contain the data
-             "  <tr><td><code>Month</code></td><td> Month for the specific record (YYYY-MM).</td></tr>",
-             "  <tr><td><code>Referral Count</code></td><td> total number of monthly referrals. (<em>Coming soon</em>)</td></tr>",
-             "  <tr><td><code>Referrals per Need</code></td><td> rate of monthly referrals per 'infant in need'. (<em>Coming soon</em>)</td></tr>",
-             "  <tr><td><code>Enrollment Count</code></td><td> total number of monthly enrollments. (<em>Coming soon</em>)</td></tr>",
-             "  <tr><td><code>Enrollments per Referral</code></td><td> rate of monthly enrollments per referral. (<em>Coming soon</em>)</td></tr>",
-             "  <tr><td><code>Visit Count</code></td><td> total number of visits for the month.</td></tr>",
-             "  <tr><td><code>Visits per Enrollment</code></td><td> rate of monthly visits per enrollment. (<em>Coming soon</em>)</td></tr>",
-             "  <tr><td><code>Visits per Need</code></td><td> rate of monthly visits per 'infant in need'.</td></tr>",
+             "  <tr><td><code>Client</code></td><td> The client's ID (which is obfuscated for this demo).</td></tr>", # <td> is standard cells that contain the data
+             "  <tr><td><code>Program</code></td><td> The program's ID (a 'program' is defined as a specific HV model adminstered from a specific site.)</td></tr>",
+             "  <tr><td><code>Visit Month</code></td><td> Month of the Visit (YYYY-MM).</td></tr>",
+             "  <tr><td><code>Phase</code></td><td>The child's developmental phase (<em>e.g.</em>, pregnant, infant, or toddler).</td></tr>",
+             "  <tr><td><code>Content Covered</code></td><td>Percent of the material covered (during the visit; 0%-100%).</td></tr>",
+             "  <tr><td><code>Involved</code></td><td>Subjective judgment how involved the client seemed (during the visit; 1-5).</td></tr>",
+             "  <tr><td><code>Conflict with Material</code></td><td>Subjective judgment how much conflict the client had with the material (during the visit; 1-5).</td></tr>",
+             "  <tr><td><code>Understands Material</code></td><td>Subjective judgment how well the client understood the material (during the visit; 1-5).</td></tr>",
+             "  <tr><td><code>Days Since Referral</code></td><td>Number of days between the referral and this visit.</td></tr>",
+             "  <tr><td><code>Final Visit?</code></td><td> Does this appear to be the client's final NFP visit?</td></tr>",
+             "  <tr><td><code><em>RR</em><sub>v1</sub></code> - <code><em>RR</em><sub>v3</sub></code></td><td>Predicted relative risk of dropping out before the next visit.  Each <em>RR</em> version corresponds to a different model.  The last one is our recommendation.</td></tr>",
              "</table>",
              "</font>")
+        
+# Client
+# Program
+# Visit Month
+# Phase
+# Content Covered
+# Client Involvement
+# Client Conflict w/ Material
+# Client  Understanding<br/>of Material
+# Days Since Referral
+# Final Visit?
+# <em>RR</em><sub>v1</sub>
+# <em>RR</em><sub>v2</sub>
+# <em>RR</em><sub>v3</sub>
+
+
       ), #End the (first) tab with the 'table' table
       # tabItem(
       #   tabName = "visits", 
